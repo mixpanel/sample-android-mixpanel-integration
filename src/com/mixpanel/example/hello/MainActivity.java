4de947f9eb782142bc9f1164ff26749ff40a945e
Joe <joe@mixpanel.com>
2012-08-27 15:46:45 -0700

    Removing unused imports

--- src/com/mixpanel/example/hello/MainActivity.java
+++ src/com/mixpanel/example/hello/MainActivity.java
@@ -4,8 +4,6 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Random;
 
 import org.json.JSONException;
